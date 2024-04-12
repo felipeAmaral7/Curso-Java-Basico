ed03e5512385a2f81ba94d04814811e21abddd14
Felipe Amaral <150284514+felipeAmaral7@users.noreply.github.com>
2024-04-11 23:03:56 -0400

    Create Main.java

---
+++ Lab34-Variaveis_e_metodos_estaticos/Exercicio01/Main.java
@@ -0,0 +1,17 @@
+package Exercicio01;
+
+public class Main{
+    public static void main(String[] args) {
+        
+        Contador.imprimeContador();
+
+        Contador.incrementar();
+
+        Contador.imprimeContador();
+
+        Contador.zerar();
+
+        Contador.imprimeContador();
+
+    }
+}
